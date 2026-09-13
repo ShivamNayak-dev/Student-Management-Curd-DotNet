2f55c33474cb0f7b59ee13a55263dc2f6e166b7d
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-09-13 14:29:55 +0530

    Add Student model

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,2 +1,70 @@
 namespace StudentManagement.Models;
 
+public class Student{
+    public int Id{
+        get;
+        set;
+    }
+
+    public string FirstName{
+        get;
+        set;
+    } = string.Empty;
+
+    public string LastName{
+        get;
+        set;
+    } = string.Empty;
+
+    public string Email{
+        get;
+        set;
+    } = string.Empty;
+
+    public string Phone{
+        get;
+        set;
+    }  = string.Empty;
+
+    public string Gender{
+        get;
+        set;
+    }  = string.Empty;
+
+    public DateTime DOB{
+        get;
+        set;
+    }
+
+    public string Course{
+        get;
+        set;
+    } = string.Empty;
+
+    public int Year{
+        get;
+        set;
+    }
+
+    public string Address{
+        get;
+        set;
+    
+    } = string.Empty;
+
+    public bool Status{
+        get;
+        set;
+    }
+
+    public DateTime CreatedAt { 
+        get; 
+        set;
+     }
+
+    public DateTime UpdatedAt { get; set; }
+
+
+
+
+}
