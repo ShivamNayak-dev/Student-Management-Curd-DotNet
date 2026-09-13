bd5dea5ed74e09f1e80425f0c95ac969619d7691
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-09-13 16:06:30 +0530

    Create Student API controller with route

---
+++ controller/StudentsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudeneMasnagement.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class StudentController : ControllerBase{
+    
+}
